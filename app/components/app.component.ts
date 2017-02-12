1615d05b54b3741917235fad2c24abacbc7768f1
pehu71 <humplikpetr@gmail.com>
2017-02-12 13:49:49 +0100

    menu

--- app/components/app.component.ts
+++ app/components/app.component.ts
@@ -1,8 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, OnInit, OnDestroy} from '@angular/core';
+import {Router} from "@angular/router";
+import {Subscription} from "rxjs";
 
 @Component({
     selector: 'my-app',
     templateUrl: 'app/tpl/app-root.html'
 })
 
-export class AppComponent { }
+export class AppComponent implements OnInit, OnDestroy {
+
+    public url: any;
+    public routerEvent: any;
+    routerSubscription: Subscription;
+
+    constructor (private router: Router) {
+
+    }
+
+    ngOnInit(): void {
+        this.routerSubscription = this.router.events
+            .subscribe(res => {
+                this.url = this.router.url;
+                this.routerEvent = res;
+            });
+    }
+
+    ngOnDestroy(): void {
+        this.routerSubscription.unsubscribe()
+    }
+}
